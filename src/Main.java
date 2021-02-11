b6dc77b8cad7f6f94f8be564d71f49ece05dff91
Martin H <ars.delectat@gmail.com>
2021-02-11 11:19:20 +0100

    Added 1st exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int age = 31;
+        char sex = 'm';
+        String firstname = "Martin";
+        String surname = "Hohla";
+        double marks = 1.9;
+        boolean married = false;
+        Date myBirthday = new Date(604882800000L);
+
+        System.out.println(firstname + " " + surname + " " + "Geschlecht: " + sex + " Married: " + married + " Notendurchschnitt: " + marks + " Birthdate: " + myBirthday);
+
     }
 }
